e469d3c1365c3af88418cb023d178760ab49d067
Matthew McGrath <rubywep@hotmail.com>
2022-03-27 14:49:07 -0700

    Add routes for TransactionsPage and NotFoundPage.

--- ReactBudget/ReactBudget/ClientApp/src/App.js
+++ ReactBudget/ReactBudget/ClientApp/src/App.js
@@ -3,19 +3,20 @@ import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-
-import './custom.css'
+import TransactionsPage from './components/TransactionsPage';
+import './custom.css';
+import NotFoundPage from './components/NotFoundPage';
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/transactions' component={TransactionsPage} />
+        <Route path='*' component={NotFoundPage} />
       </Layout>
     );
   }
