26df6819f996ec62228321e15e38bc9fd26ac2f5
Matthew McGrath <rubywep@hotmail.com>
2022-03-27 14:45:09 -0700

    Remove Bootstrap references and components

--- ReactBudget/ReactBudget/ClientApp/src/components/Layout.js
+++ ReactBudget/ReactBudget/ClientApp/src/components/Layout.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
 
 export class Layout extends Component {
   static displayName = Layout.name;
 
-  render () {
+  render() {
     return (
       <div>
         <NavMenu />
-        <Container>
-          {this.props.children}
-        </Container>
+        <div>{this.props.children}</div>
       </div>
     );
   }
