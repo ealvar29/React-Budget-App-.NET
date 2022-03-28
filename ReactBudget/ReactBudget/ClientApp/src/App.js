9a630d2bd2f792ab4c1afc41822c457c936a6a91
Matthew McGrath <rubywep@hotmail.com>
2022-03-28 09:25:32 -0700

    Upgrade to React Router v6 & mod App.js to use it

--- ReactBudget/ReactBudget/ClientApp/src/App.js
+++ ReactBudget/ReactBudget/ClientApp/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route, Routes } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import TransactionsPage from './components/TransactionsPage';
 import NotFoundPage from './components/NotFoundPage';
-import './custom.css';
+import './index.css';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -13,10 +13,12 @@ export default class App extends Component {
   render() {
     return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/transactions' component={TransactionsPage} />
-        <Route path='*' component={NotFoundPage} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/fetch-data' element={<FetchData />} />
+          <Route path='/transactions' element={<TransactionsPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
       </Layout>
     );
   }
