90c9d460f4ab39f738d7695e1e0405c4454a97bc
Matthew McGrath <rubywep@hotmail.com>
2022-03-31 23:08:09 -0700

    Add simple 404 page

--- ReactBudget/ReactBudget/ClientApp/src/components/NotFoundPage.jsx
+++ ReactBudget/ReactBudget/ClientApp/src/components/NotFoundPage.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 function NotFoundPage() {
-  return <div>Page Not Found</div>;
+  return (
+    <div class='bg-indigo-900 relative overflow-hidden h-screen'>
+      <div class='inset-0 bg-black opacity-25 absolute'></div>
+      <div class='container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40'>
+        <div class='w-full font-mono flex flex-col items-center relative z-10'>
+          <h1 class='font-extrabold text-5xl text-center text-white leading-tight mt-4'>
+            Page Not Found
+          </h1>
+          <p class='font-extrabold text-8xl my-44 text-white animate-bounce'>
+            404
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NotFoundPage;
